2d35c2570301d2a254f6376bcce81182bcd553b4
ddolan88 <104475059+ddolan88@users.noreply.github.com>
2022-05-15 11:46:25 -0700

    creating variables
    We were creating variables with var, let, and const

---
+++ script.js
@@ -0,0 +1,36 @@
+//alert("Hello World!");
+//prompt("Enter your name:");
+//console.log("This is a message");
+//console.warn("This is a warning");
+//console.error("This is an error");
+
+// variables
+
+var x; //declaration of the variable
+x=10; // assignation of the value
+//document.writeIn(x);
+
+let userAge = 99;
+userAge=98;
+document.write(userAge);
+var userName = "kyle";//declaration and assignation
+
+document.write(`<p>The users name is: ${userName}, and the age is ${userAge}</p>`);
+
+const taxes= 11;
+document.write(taxes);
+
+let A=0;
+let B=`Jane`;
+let C=`Somewhere`;
+let D=`Dev`;
+
+
+document.write(`<p> You will be a ${D} in ${C}, and married to ${B} with ${A} kids.`)
+
+
+
+
+
+
+
